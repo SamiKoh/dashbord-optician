a911d482fae56973cfe159b00329c23931c26d03
Sami Kohvakka <sami.kohvakka@live.com>
2021-11-17 13:20:40 +0200

    Fix layout issues on customer form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,70 +1,76 @@
 <template>
   <b-container class="home">
-
-
     <b-row>
-      <!--  <b-col cols="4">
-        <b-card> </b-card>
-        Left
-      </b-col> -->
       <b-col cols="12">
-        <b-card>
+        <b-card class="mb-5">
           <h2>Eyesight test form</h2>
           <b-form>
-            <b-form-group label="First name">
-              <b-form-input placeholder="Please type your first name">
+            <b-form-group
+              label-for="firstname"
+              class="label"
+              label="First name"
+            >
+              <b-form-input
+                id="firstname"
+                placeholder="Please type your first name"
+              >
               </b-form-input>
             </b-form-group>
 
-            <b-form-group label="Last name">
+            <b-form-group class="label" label="Last name">
               <b-form-input placeholder="Please type your last name">
               </b-form-input>
             </b-form-group>
 
-            <b-form-group label="Email Address">
-              <b-form-input placeholder="Please type your email address" type="email">
+            <b-form-group class="label" label="Email Address">
+              <b-form-input
+                placeholder="Please type your email address"
+                type="email"
+              >
               </b-form-input>
             </b-form-group>
-            <b-form-group label="Phone number">
-              <b-form-input placeholder="Please type your phone number" >
+            <b-form-group class="label" label="Phone number">
+              <b-form-input placeholder="Please type your phone number">
               </b-form-input>
             </b-form-group>
-            <b-form-group label="Address">
+            <b-form-group class="label" label="Address">
               <b-form-input placeholder="Please type your name"> </b-form-input>
             </b-form-group>
-            <b-form-group label="Postcode">
+            <b-form-group class="label" label="Postcode">
               <b-form-input placeholder="Please type your postcode">
               </b-form-input>
             </b-form-group>
-            <b-form-group label="City">
-              <b-form-input placeholder="Please type your city">
-              </b-form-input>
+            <b-form-group class="label" label="City">
+              <b-form-input placeholder="Please type your city"> </b-form-input>
             </b-form-group>
-            <b-form-group label="State">
+            <b-form-group class="label" label="State">
               <b-form-input placeholder="Please type your state">
               </b-form-input>
             </b-form-group>
-            <b-form-group label="Date of birth">
+            <b-form-group class="label" label="Date of birth">
               <b-form-input type="date"> </b-form-input>
             </b-form-group>
 
-            <b-form-group label="Lisence number">
+            <b-form-group class="label" label="Lisence number">
               <b-form-input
                 placeholder="Please type your lisence number (if known)"
               >
               </b-form-input>
             </b-form-group>
 
-            <b-form-group label="Lisence class">
-              <b-form-checkbox-group>
-                <b-form-checkbox id="checkbox-1"> A </b-form-checkbox>
-                <b-form-checkbox id="checkbox-1"> B </b-form-checkbox>
-                <b-form-checkbox id="checkbox-1"> C </b-form-checkbox>
-                <b-form-checkbox id="checkbox-1"> D </b-form-checkbox>
-                <b-form-checkbox id="checkbox-1"> E </b-form-checkbox>
-                <b-form-checkbox id="checkbox-1"> F </b-form-checkbox>
+            <b-form-group id="cb" class="label" label="Lisence class">
+              <b-form-checkbox-group v-model="checkbox">
+                <b-form-checkbox value="A" id="checkbox-1"> A </b-form-checkbox>
+                <b-form-checkbox value="B" id="checkbox-2"> B </b-form-checkbox>
+                <b-form-checkbox value="C" id="checkbox-3"> C </b-form-checkbox>
+                <b-form-checkbox value="D" id="checkbox-4"> D </b-form-checkbox>
+                <b-form-checkbox value="E" id="checkbox-5"> E </b-form-checkbox>
+                <b-form-checkbox value="F" id="checkbox-6"> F </b-form-checkbox>
               </b-form-checkbox-group>
-              <b-form-input placeholder="Subclass information"></b-form-input>
+              <b-form-input
+                class="mt-3"
+                placeholder="Subclass information"
+              ></b-form-input>
             </b-form-group>
 
             <b-list-group>
@@ -73,24 +79,24 @@
                 identified? If YES, please tick:
 
                 <b-form-checkbox-group>
-                  <b-form-checkbox id="checkbox-1"> Diplopia </b-form-checkbox>
-                  <b-form-checkbox id="checkbox-1">
+                  <b-form-checkbox id="checkbox-7"> Diplopia </b-form-checkbox>
+                  <b-form-checkbox id="checkbox-8">
                     Monocular Vision
                   </b-form-checkbox>
-                  <b-form-checkbox id="checkbox-1">
+                  <b-form-checkbox id="checkbox-9">
                     Visual Field Defec
                   </b-form-checkbox>
-                  <b-form-checkbox id="checkbox-1">
+                  <b-form-checkbox id="checkbox-10">
                     Retinitis Pigmentosa
                   </b-form-checkbox>
                 </b-form-checkbox-group>
               </b-list-group-item>
             </b-list-group>
+            <div class="mt-3 mb-3">
 
-            
-              <b-button variant="success">Send</b-button>
-              <b-button class="ml-3">Clear</b-button>
-            
+            </div>
+            <b-button variant="success">Send</b-button>
+            <b-button class="ml-3">Clear</b-button>
           </b-form>
         </b-card>
       </b-col>
@@ -104,6 +110,15 @@ import Vue from "vue";
 export default Vue.extend({
   name: "Home",
   components: {},
-  data: () => ({}),
+  data: () => ({
+    checkbox: [],
+  }),
 });
 </script>
+
+
+<style scoped>
+.form-group {
+  text-align: left !important;
+}
+</style>
